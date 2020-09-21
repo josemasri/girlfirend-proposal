c133ccf3ae66b701f71e90b468ba2d168f96fd64
Jose Masri <josemasri2@gmail.com>
2020-09-21 11:40:02 -0500

    Added TypedJs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Typed from "react-typed";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typed
+        strings={[
+          "El día de hoy, lunes 21 de septiembre quiero preguntarte algo",
+          "Primero quiero decirte que eres la niña más hermosa del mundo",
+          "Que estar contigo me hace sonreir",
+          "Que desde que llegaste a mi vida todo es mejor",
+          "Me encanta todo de tí, tu sonrira, tus ojos, tu pelo, tu piel, tu cuerpo, todo de tí es perfecto",
+          "Adoro platicar contigo y pasar mucho tiempo a tú lado. Ver pelis o solo verte, no necesito nada más",
+          "No solo físicamente eres grandiosa, tu forma de ser y esa chispa que tienes me encanta",
+          "Me gustan mucho tus locuras",
+          "Me encanta que no seas predecible",
+          "Y me encantaría poder pasar mucho más tiempo contigo",
+          "Despues de todo esto, quiero preguntarte algo",
+          "¿Quieres ser mi novia?\n❤❤❤",
+        ]}
+        typeSpeed={80}
+      />
     </div>
   );
 }
